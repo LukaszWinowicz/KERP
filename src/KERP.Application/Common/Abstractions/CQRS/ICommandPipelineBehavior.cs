7833394920bea7766984f17ab15b4fbd758d9d5e
LukaszWinowicz <lwinowicz@gmail.com>
2025-10-14 15:06:49 +0200

    refactor(Application): Unify pipeline building logic in Mediator
    Refactored the Mediator implementation to eliminate code duplication between SendCommandAsync and SendQueryAsync.

--- src/KERP.Application/Common/Abstractions/CQRS/ICommandPipelineBehavior.cs
+++ src/KERP.Application/Common/Abstractions/CQRS/ICommandPipelineBehavior.cs
@@ -6,23 +6,8 @@
 /// wykonanie logiki przed i po wywołaniu właściwego handlera komendy.
 /// </summary>
 /// <typeparam name="TCommand">Typ komendy implementującej ICommand lub ICommand<TResult>.</typeparam>
-/// <typeparam name="TResponse">Typ odpowiedzi - zazwyczaj Result lub Result<TValue>.</typeparam>
+/// <typeparam name="TResponse">Typ odpowiedzi - zazwyczaj Result lub Result&lt;TValue&gt;.</typeparam>
 public interface ICommandPipelineBehavior<in TCommand, TResponse>
+    : IPipelineBehavior<TCommand, TResponse>
 {
-    /// <summary>
-    /// Wykonuje logikę behavior'a w pipeline'ie dla komendy.
-    /// </summary>
-    /// <param name="command">Komenda przechodząca przez pipeline.</param>
-    /// <param name="next">Delegat do następnego ogniwa w łańcuchu.
-    /// Wywołanie next() uruchamia kolejny bahavior lub właściwy handler.
-    /// Można NIE wywoływać next() aby przerwać łańcuch (np. przy błędzie walidacji).</param>
-    /// <param name="cancellationToken">Token anulowania operacji, propagowany przez cały pipeline.</param>
-    /// <returns>Odpowiedź z pipeline'u. Behavior może:
-    /// * Przekazać odpowiedź bez zmian
-    /// * Zmodyfikować odpowiedź
-    /// * Zwracać własną odpowiedź (np. Result.Failure przy błędzie).</returns>
-    Task<TResponse> HandleAsync(
-        TCommand command,
-        Func<Task<TResponse>> next,
-        CancellationToken cancellationToken = default);
 }
---
+++ src/KERP.Application/Common/Abstractions/CQRS/IPipelineBehavior.cs
@@ -0,0 +1,17 @@
+﻿namespace KERP.Application.Common.Abstractions.CQRS;
+
+/// <summary>
+/// Generyczny interfejs dla pipeline behavior, z którego dziedziczą
+/// ICommandPipelineBehavior oraz IQueryPipelineBehavior.
+/// </summary>
+public interface IPipelineBehavior<in TRequest, TResponse>
+{
+    /// <summary>
+    /// Wykonuje logikę behavior'a w pipeline'ie.
+    /// </summary>
+    /// <returns></returns>
+    Task<TResponse> HandleAsync(
+        TRequest request,
+        Func<Task<TResponse>> next,
+        CancellationToken cancellationToken = default);
+}
